1cd0613817629fbb4428173cc2a469496ae539a3
crapboy <giulianopertile@gmail.com>
2021-03-07 14:55:32 -0300

    cambiado el allowed host y fix delete template

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'ebb4*&(=6vs#o(e9j39x-8!hzq4z4o8)58@-q0p@y92_p9n2h3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stark-earth-93151.herokuapp.com', '*']
+ALLOWED_HOSTS = ['stark-earth-93151.herokuapp.com',
+                 'halloffamevideos.herokuapp.com']
 
 
 # Application definition
